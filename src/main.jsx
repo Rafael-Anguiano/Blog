8f79dff4c4c2f253911a27404717fb701f84b910
Rafael Anguiano <rj.anguiano.23@gmail.com>
2025-04-10 15:17:33 -0600

    Installed Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Feed from './components/Feed.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
