6457b7c51c338b5f29452292335b132b39e2af53
Rafael Anguiano <rj.anguiano.23@gmail.com>
2025-04-10 15:09:21 -0600

    Added React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Feed from './components/Feed.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="/" element={<Feed />} />
+        </Route>
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
