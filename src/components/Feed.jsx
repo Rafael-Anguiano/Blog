86b78702d2913fe715fc3ebe2b213aedee1e2b10
Hector Mariscal <hectorenrique.mariscalsanchez@umg.edu.mx>
2025-04-11 08:46:16 -0600

    Cambios a header

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,10 @@
+import Post from './Post'
 const Feed = () => {
-  return (
-    <section className="bg-black">
-      <Post />
-    </section>
-  )
+    return (
+        <section className="bg-black">
+            <Post />
+        </section>
+    )
 }
 
 export default Feed
